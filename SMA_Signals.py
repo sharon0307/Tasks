c7b045c5dc2bce71979e38edb67f5cf415ea856e
sharon0307 <156105998+sharon0307@users.noreply.github.com>
2024-02-05 19:23:33 +0530

    Add files via upload

---
+++ SMA_Signals.py
@@ -0,0 +1,50 @@
+import os
+import pandas as pd
+import numpy as np
+
+# Set the current working directory
+cwd = os.getcwd()
+
+# Specify the filename
+filename = "RELIANCE_1m (1).csv"
+
+# Create the full file path
+file_path = os.path.join(cwd, filename)
+
+# Read the CSV file into a DataFrame
+df = pd.read_csv(file_path)
+window = 50
+# Calculate moving averages
+df['SMA' + str(window)] = df['close'].rolling(window).mean()
+
+# Generate signals
+df['signal'] = np.where(df['close'] > df['SMA' + str(window)], 1.0, 0.0)
+df['position'] = df['signal'].diff()
+
+# Create 'position' column based on 'signal'
+df['BE/SE'] = ''
+
+# Update 'BE/SE' based on 'position'
+df.loc[df['position'] == -1, 'BE/SE'] = 'BE'
+df.loc[df['position'] == 1, 'BE/SE'] = 'SE'
+
+res = df['SMA' + str(window)].first_valid_index()
+
+# Update DataFrame based on the condition
+if res is not None:
+    df.loc[res, ['position', 'BE/SE']] = np.nan
+
+    # Additional logic to ensure it runs only once
+
+# Find the first occurrence of 'SE' or 'BE'
+first_value = df.index[(df['BE/SE'] == 'SE') | (df['BE/SE'] == 'BE')].min()
+
+df.at[first_value, 'BE/SE'] = ''
+
+df['signal'] = np.where(df['position'] == 1, 'Buy', np.where(df['position'] == -1, 'Sell', ''))
+
+# Drop unnecessary columns
+df = df.drop(columns=['position'])
+
+# Save the DataFrame to a new CSV file
+df.to_csv("SMA_with_Signals.csv", index=False)
